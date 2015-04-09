51d491203ddbc4a6052f4de0dcdd7153eb92cf7b
Rajendra Gadhi <rajungandhi@yahoo.com>
2015-04-09 17:59:46 +0530

    Added expense class

---
+++ ProjectB/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectB.Models
+{
+    public class Expense
+    {
+        public Expense()
+        {
+            //15.1 related changes
+        }
+        public int ExpenseID { get; set; }
+    }
+}
